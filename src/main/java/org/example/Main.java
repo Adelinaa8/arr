d0a7198f92a93eb011d4b2d6f1d512e694a1ee1b
Adelina <adelshnv@gmail.com>
2023-03-15 13:18:54 +0300

    arr

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 18, 7, 14, 14, 16};
+        int minAverageSales = service.minAverageSales(sales);
+
+        System.out.println(minAverageSales);
+
+
+    }
+
+
+}
